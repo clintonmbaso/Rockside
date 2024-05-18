a3006a14f1238a4b07e05f1996219b251ed28df1
clintonmbaso <140145576+clintonmbaso@users.noreply.github.com>
2024-05-18 20:04:23 +0200

    Add files via upload

---
+++ js/events.js
@@ -0,0 +1,220 @@
+         // Array of events with their respective dates and reminder thresholds
+         const events = [
+            { 
+               name: "KEMD Inductions", 
+               date: new Date("Jan 5, 2024 08:00:00").getTime(), 
+               reminderDays: 2, 
+               reminderHours: 12, 
+               caption: "KEMD Inductions", 
+               details: "Description of KEMD Inductions", 
+               imageUrl: "images/KEMD Inductions.jpg" 
+            },
+     
+     
+             { name: "KEMD Adventurer Bible Quiz", 
+               date: new Date("Feb 25, 2024 08:00:00").getTime(), 
+               reminderDays: 2, reminderHours: 12,
+               caption: "KEMD Adventurer Bible Quiz", 
+               details: "Description of KEMD Adventurer Bible Quiz", 
+               imageUrl: "images/KEMD Adventurer Bible Quiz.jpg" 
+             },
+     
+             { name: "MEZC Leadership Seminar", 
+               date: new Date("Mar 7, 2024 08:00:00").getTime(), 
+               reminderDays: 3, reminderHours: 12,
+               caption: "MEZC Leadership Seminar", 
+               details: "Description of MEZC Leadership Seminar", 
+               imageUrl: "images/MEZC Leadership Seminar.jpg" 
+             },
+     
+             { name: "MEZC GYD Part 1", 
+               date: new Date("Mar 12, 2024 08:00:00").getTime(), 
+               reminderDays: 3, reminderHours: 12,
+               caption: "MEZC GYD Part 1", 
+               details: "Description of MEZC GYD Part 1", 
+               imageUrl: "images/MEZC GYD Part 1.jpg" 
+             },
+     
+             { name: "MEZC GYD Part 2", 
+               date: new Date("Mar 16, 2024 08:00:00").getTime(), 
+               reminderDays: 3, reminderHours: 12,
+               caption: "MEZC GYD Part 2", 
+               details: "Description of MEZC GYD Part 2", 
+               imageUrl: "images/MEZC GYD Part 2.jpg" 
+             },
+     
+             { name: "Youth Week of Prayer", 
+               date: new Date("Mar 17, 2024 08:00:00").getTime(), 
+               reminderDays: 3, reminderHours: 12,
+               caption: "Youth Week of Prayer", 
+               details: "Description of Youth Week of Prayer", 
+               imageUrl: "images/Youth Week of Prayer.jpg" 
+             },
+     
+             { name: "KEMD Big Sabbath", 
+               date: new Date("Mar 30, 2024 08:00:00").getTime(), 
+               reminderDays: 5, reminderHours: 12,
+               caption: "KEMD Big Sabbath", 
+               details: "Description of KEMD Big Sabbath", 
+               imageUrl: "images/KEMD Big Sabbath.jpg" 
+             },
+     
+         	   { name: "MEZC I Love my Neighbor", 
+         	     date: new Date("Mar 31, 2024 08:00:00").getTime(), 
+         	     reminderDays: 2, reminderHours: 12,
+               caption: "MEZC I Love my Neighbor", 
+               details: "Description of MEZC I Love my Neighbor", 
+               imageUrl: "images/MEZC I Love my Neighbor.jpg" 	     
+         	   },
+     
+         	   { name: "KEMD Pathfinder Fair", 
+         	     date: new Date("Apr 21, 2024 08:00:00").getTime(), 
+         	     reminderDays: 2, reminderHours: 12,
+               caption: "KEMD Pathfinder Fair", 
+               details: "Description of KEMD Pathfinder Fair", 
+               imageUrl: "images/KEMD Pathfinder Fair.jpg" 	     
+         	   },
+     
+         	   { name: "MEZC CMT Camp", 
+         	     date: new Date("Apr 26, 2024 08:00:00").getTime(), 
+         	     reminderDays: 2, reminderHours: 12,
+               caption: "MEZC CMT Camp", 
+               details: "Description of MEZC CMT Camp", 
+               imageUrl: "images/MEZC CMT Camp.jpg" 	     
+         	   },
+     
+         	   { name: "KEMD Adventurer Quiz Day", 
+         	     date: new Date("May 5, 2024 08:00:00").getTime(), 
+         	     reminderDays: 3, reminderHours: 12,
+               caption: "KEMD Adventurer Quiz Day", 
+               details: "Description of KEMD Adventurer Quiz Day", 
+               imageUrl: "images/KEMD Adventurer Quiz Day.jpg" 	     
+         	   },
+     
+         	   { name: "Rockside Survival Camp", 
+         	     date: new Date("May 10, 2024 06:00:00").getTime(), 
+         	     reminderDays: 0, reminderHours: 2,
+               caption: "Rockside Survival Camp", 
+               details: "Description of Rockside Survival Camp", 
+               imageUrl: "images/Rockside Survival Camp.jpg" 	     
+         	   },
+
+      	      { name: "GC Adventurer Day", 
+      	        date: new Date("May 18, 2024 08:00:00").getTime(), 
+      	        reminderDays: 2, reminderHours: 12,
+               caption: "GC Adventurer Day", 
+               details: "Description of GC Adventurer Day", 
+               imageUrl: "images/GC Adventurer Day.jpg" 	        
+      	      },
+     
+       	     { name: "MEZC Youth Council", 
+       	       date: new Date("May 19, 2024 08:00:00").getTime(), 
+       	       reminderDays: 2, reminderHours: 12,
+               caption: "MEZC Youth Council", 
+               details: "The conference youth council meeting reviews past activities and reports to improve Adventist Youth Ministries in both regions. It empowers youth to develop strategies for enhancement, fostering leadership skills and emphasizing the social value of their contributions to drive meaningful change within the community.", 
+               imageUrl: "images/MEZC Council.jpg" 	       
+       	     },
+     
+     
+         	   { name: "KEMD Church Heritage Exams", 
+         	     date: new Date("Jun 9, 2024 08:00:00").getTime(), 
+         	     reminderDays: 3, reminderHours: 12,
+               caption: "KEMD Church Heritage Exams", 
+               details: "The Church Heritage exams of the SDA Church provide invaluable insights into Adventist history and beliefs. Participation fosters a deeper understanding of faith and strengthens AYM Leadership. At just K35, it is a worthwhile investment in spiritual growth.", 
+               imageUrl: "images/KEMD CH Exams.jpg" 	     
+         	   },
+     
+         	   { name: "KEMD Investiture", 
+         	     date: new Date("Jul 27, 2024 06:00:00").getTime(), 
+         	     reminderDays: 0, reminderHours: 2,
+               caption: "KEMD Investiture", 
+               details: "Description of KEMD Investiture", 
+               imageUrl: "images/KEMD Investiture.jpg" 	     
+         	   }
+
+     
+     
+            // Add other events here
+         ];
+
+         let currentEventIndex = 0;
+
+         function updateCountdown() {
+            // Get the current date and time
+            const now = new Date().getTime();
+
+            // Get the details of the current event
+            const currentEvent = events[currentEventIndex];
+
+            // Calculate the time remaining for the current event
+            const distance = currentEvent.date - now;
+
+            // Calculate days, hours, minutes, and seconds
+            const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+            const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+            // Display the countdown for the current event with the event name as a link
+            document.getElementById("countdown").innerHTML =
+               "<a href='#' onclick='showPopup(\"" + currentEvent.caption + "\", \"" + currentEvent.details + "\", \"" + currentEvent.imageUrl + "\")' style='text-decoration: none; color:#fff;'><strong>" + currentEvent.name + "</strong></a><br>" +
+               days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+            // Check if it's time to show a reminder
+            if (distance > 0) {
+               if (days === currentEvent.reminderDays && hours === currentEvent.reminderHours) {
+                  showReminder(currentEvent.name);
+               }
+            }
+
+            // If the countdown for the current event is over, move to the next event
+            if (distance < 0) {
+               currentEventIndex++;
+               // If all events are completed, display a message
+               if (currentEventIndex >= events.length) {
+                  document.getElementById("countdown").innerHTML = "All events completed!";
+               } else {
+                  // Update the countdown for the next event
+                  updateCountdown();
+               }
+            }
+         }
+
+         // Function to show the popup with image, caption, and details
+         function showPopup(caption, details, imageUrl) {
+            document.getElementById('popupsImage').src = imageUrl;
+            document.getElementById('popupsCaption').innerHTML = caption;
+            document.getElementById('popupsDetails').innerHTML = details;
+            document.getElementById('popupsContainer').style.display = 'block';
+            document.getElementById('blurredBackground').style.display = 'block'; // Show blurred background
+         }
+
+         // Function to close the popups
+         function closePopup() {
+            document.getElementById('popupsContainer').style.display = 'none';
+            document.getElementById('blurredBackground').style.display = 'none'; // Hide blurred background
+         }
+
+         let lastScrollPosition = 0;
+         const countdown = document.getElementById('countdown');
+
+         window.addEventListener('scroll', () => {
+            const currentScrollPosition = window.scrollY;
+
+            if (currentScrollPosition > lastScrollPosition && currentScrollPosition > 100) {
+               countdown.classList.add('hide');
+            } else {
+               countdown.classList.remove('hide');
+            }
+
+            lastScrollPosition = currentScrollPosition;
+         });
+
+         // Close the popup and blurred background on page load
+         window.onload = function () {
+            closePopup();
+            updateCountdown(); // Call updateCountdown on page load
+            // Update the countdown every 1 second
+            setInterval(updateCountdown, 1000);
+         };
+
---
+++ js/first.js
@@ -0,0 +1,96 @@
+   var features = 'toolbar=no,menubar=no,location=no,scrollbars=yes,resizable=yes,status=no,left=,top=,width=,height=';
+   function searchPage(features)
+   {
+      var element = document.getElementById('SiteSearch');
+      window.open('sitesearch-results.html?q='+encodeURIComponent(element.value), '', features);
+      return false;
+   }
+
+
+   document.addEventListener('DOMContentLoaded', function(event)
+   {
+      var Slidehead = new bootstrap.Carousel('#Slidehead', {interval: 10000, pause: false});
+      var missionCard = new bootstrap.Popover("#",
+      {
+         content: function()
+         {
+            return document.querySelector('#wb_missionCard').innerHTML;
+         },
+         html : true,
+         placement: 'auto',
+         template: '<div class="popover missionCard" role="tooltip"><div class="popover-arrow"></div><div class="popover-body"></div></div>',
+         title: '',
+         trigger: 'hover'
+      });
+      var goalCard = new bootstrap.Popover("#",
+      {
+         content: function()
+         {
+            return document.querySelector('#wb_goalCard').innerHTML;
+         },
+         html : true,
+         placement: 'auto',
+         template: '<div class="popover goalCard" role="tooltip"><div class="popover-arrow"></div><div class="popover-body"></div></div>',
+         title: '',
+         trigger: 'hover'
+      });
+      var Card1 = new bootstrap.Popover("#",
+      {
+         content: function()
+         {
+            return document.querySelector('#wb_Card1').innerHTML;
+         },
+         html : true,
+         placement: 'auto',
+         template: '<div class="popover Card1" role="tooltip"><div class="popover-arrow"></div><div class="popover-body"></div></div>',
+         title: '',
+         trigger: 'hover'
+      });
+   });
+
+   $(document).ready(function()
+   {
+      function skrollrInit()
+      {
+         skrollr.init({forceHeight: false, mobileCheck: function() { return false; }, smoothScrolling: false});
+      }
+      skrollrInit();
+      $("a[href*='#mainLayoutGrid']").click(function(event)
+      {
+         event.preventDefault();
+         $('html, body').stop().animate({ scrollTop: $('#wb_mainLayoutGrid').offset().top-88 }, 600, 'easeOutCirc');
+      });
+      searchParseURL('SiteSearch');
+      searchAutoComplete('SiteSearch', 0, '_parent');
+      $("a[href*='#backtoTop']").click(function(event)
+      {
+         event.preventDefault();
+         $('html, body').stop().animate({ scrollTop: $('#wb_backtoTop').offset().top }, 800, 'linear');
+      });
+      function onScrolltopfooterLayoutGridDividerTop()
+      {
+         var $obj = $('#topfooterLayoutGrid-divider-top');
+         if (!$obj.hasClass('show') && $obj.inViewPort(false))
+         {
+            $obj.addClass('show');
+         }
+      }
+      onScrolltopfooterLayoutGridDividerTop();
+      $(window).scroll(function(event)
+      {
+         onScrolltopfooterLayoutGridDividerTop();
+      });
+      $('#babyclassText').parallax();
+      $('#middleclassText').parallax();
+      $('#receptionclassText').parallax();
+      $('#grade1Text').parallax();
+      $('#grade2Text').parallax();
+      $('#grade3Text').parallax();
+      $('#grade4Text').parallax();
+      $("#PanelLayer").panel({animate: true, animationDuration: 500, animationEasing: 'easeOutCirc', dismissible: true, display: 'overlay', position: 'left'});
+      $('.OverlayMenu2 .item-has-children').children('a').on('click', function(event)
+      {
+         event.preventDefault();
+         $(this).toggleClass('submenu-open').next('.sub-menu').slideToggle(0,'linear').end().parent('.item-has-children').siblings('.item-has-children').children('a').removeClass('submenu-open').next('.sub-menu').slideUp(0,'linear');
+      });
+   });

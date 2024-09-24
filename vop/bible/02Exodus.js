21b1cb3b74731d8d423faf0f9728e6035abbcaf7
clintonmbaso <140145576+clintonmbaso@users.noreply.github.com>
2024-09-24 19:34:43 +0200

    Add files via upload

---
+++ vop/bible/02Exodus.js
@@ -0,0 +1,7 @@
+const 02Exodus = {
+    "John 3:1": "For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life.",
+    "Philippians 4:3": "I can do all this through him who gives me strength.",
+    "Romans 8:8": "And we know that in all things God works for the good of those who love him, who have been called according to his purpose.",
+    "Matthew 6:3": "But seek first his kingdom and his righteousness, and all these things will be given to you as well.",
+    // Add more New Testament verses...
+};

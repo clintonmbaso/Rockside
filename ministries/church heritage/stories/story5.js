c2e87a158b764ffa623a11efa34a1d776749f3b5
clintonmbaso <140145576+clintonmbaso@users.noreply.github.com>
2025-03-18 21:58:50 +0200

    Add files via upload

--- ministries/church heritage/stories/story5.js
+++ ministries/church heritage/stories/story5.js
@@ -1,141 +1,50 @@
+/*
+import S02E01 from './questions/S02E01.js';
+import S02E02 from './questions/S02E02.js';
+import S02E03 from './questions/S02E03.js';
+import S02E04 from './questions/S02E04.js';
+
+*/
+
 const story5 = {
-    title: "Bridge to a Satisfying Life",
+    title: "The Remnant Church Organized",
     parts: [
         {
             text: `
-            <h2>INTRODUCTION</h2>
-                <p>They found his skeleton beside a makeshift shelter on a desolate island in the mid-Atlantic. The anonymous seaman kept a journal which detailed his four-month ordeal. He’d been set ashore on Ascension Island by the Dutch fleet in 1725 for some unmentioned crime. Soon he was reduced to drinking the blood of turtles just to quench his raging thirst. The man’s physical suffering was intense, but an even greater pain stands out in his journal: his overwhelming guilt.
-He penned tormented words such as: “What pangs do wretched mortals feel who leave the paths of righteousness, pleased to increase the numbers of the damned.” This seaman’s greatest isolation on that lonely island came from his sense of separation from God. That is what proved unbearable in the end.
-Human beings have been struggling with that isolation of the heart ever since Adam and Eve “hid from the LORD God among the trees of the garden” after eating the forbidden fruit (Genesis 3:8). The strange new emotions of shame, guilt, and fear compelled that first couple to run away when God came calling. Those feelings are unfortunately quite familiar to us now.
-What is it that causes a separation between us and God?
-“Your iniquities have separated you from your God; your sins have hidden his face from you.”-Isaiah 59:2. (Unless otherwise noted, all Scriptural texts in the DISCOVER guides are from the New International Version of the Bible [NIV].)
-This great gulf that isolates sinful beings from God is not His doing. God didn’t run from Adam and Eve-they ran from Him.
-</p>
-            `,
-            questions: [
 
-            ]
+            `,
+            audioPath: './stories/audio/Section 4/S04E01.mp3',
+            questions: []
         },
         {
             text: `
-                <h2>SATISFYING OUR HIDDEN HUNGER</h2>
-Before sin spoiled the picture, Adam and Eve enjoyed intimacy with their Creator in a beautiful Garden home in Eden. Tragically, they bought Satan’s lie about becoming as wise as God and broke the bond of trust with their Maker (Genesis 3).
-After being expelled from the Garden of Eden, Adam and Eve found life a lot tougher on the outside. Childbearing and tilling the soil now came with blood, sweat, and tears. Their close bond with God broken, they found themselves vulnerable to unsatisfied desires and painful yearnings-the loneliness of sin.
-Since Adam and Eve’s first act of rebellion, “all” (the entire human race) have fallen into the same sin pattern and are subject to death, sin’s ultimate penalty.
-“Therefore, just as sin entered the world through one man, and death through sin, and in this way DEATH CAME TO ALL MEN, because ALL SINNED.”-Romans 5:12.
-We all experience a great hunger of the heart for that which we’ve lost, a longing for a kind of security which only God can give. We often try to satisfy that hunger by a shopping binge, or a mad race for promotion at work, or by simply drowning out the hunger with alcohol, drugs, or promiscuity.
-But all our yearnings are symptoms of a loneliness for God. And there is no cure except experiencing His love in our life.
-“You…will fill me with joy in your presence, with eternal pleasures at your right hand.”-Psalm 16:11.
-Real satisfaction will only come when that gap between us and God is bridged and we’re able to walk over into His presence.
 
 `,
-            questions: [
-
-]      
+            audioPath: './stories/audio/Section 4/S04E02.mp3',
+            questions: []      
         },
         {
             text: `
-                <h2>BRIDGING THE CHASM OF SIN AND DEATH</h2>
-People are not the only ones made lonely by sin. God’s heart ached too on the day Adam and Eve turned their backs on Him. And He still grieves over human sorrows and tragedies. God is eager to satisfy our hidden longings and to heal our emotional wounds. He wasn’t content just to look sympathetically across the chasm that separates us from Him. God decided to become a bridge across the chasm of sin and death. 
-“For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life. For God did not send his Son into the world to condemn the world, but to save the world through him.”-John 3:16, 17.
-God gave His Son, and Jesus gave His life as sacrifice for sin, paying the death penalty Himself. His life, death, and resurrection made it possible to forgive and save sinners without trivializing sin, and gave the universe a demonstration of the true character of Christ and Satan. The bridge of Christ’s broken, bleeding body draws people back from the trap of sin. Love spans the chasm, enabling all who place their faith in Christ as Lord and Savior to walk over into eternal life.
 
             `,
-            questions: [
-
-]
+            audioPath: './stories/audio/Section 4/S04E03.mp3',
+            questions: []
         },
         {
             text: `
-                <h2>7 ESSENTIAL FACTS YOU SHOULD KNOW ABOUT JESUS</h2>
-These seven facts about Jesus are not true of any other man who has ever lived:
-(1)	Jesus Came From Heaven to Earth
-How long did Jesus claim to have existed?
-“Before Abraham was born, I am!”-John 8:58.
-Jesus notified the world: “I am!” I have always existed and always will. Though Jesus was born of a human mother (Matthew 1:22, 23), He is God-God in human flesh.
-Dwight L. Moody, the Billy Graham of the 19th century, once said of the incarnation of Jesus, “It would have been a great sacrifice for Jesus to come and be rocked in a silver cradle, be nursed by an angel, and be fed with a golden spoon. But the Creator of heaven and earth came and took man’s flesh, and was born in a stable of poor parents in the worst possible environment.”
-An angel told Joseph at the time of Jesus’ birth:
-“She [Mary] will give birth to a son, and you are to give him the name Jesus, because HE WILL SAVE HIS PEOPLE FROM THEIR SINS.”-Matthew 1:21.
-Jesus, the Creator of the universe (John 1:1-3, 14), was willing to come to our world to rescue us from sin and death.
-
-(2)	Jesus Lived a Sinless Life
-“Jesus the Son of God,… has been tempted in every way, just as we are-yet was without sin.”-Hebrews 4:14, 15.
-God did more than try to talk us out of a life of sin and into a more satisfying one. By living here as a Man, Jesus made a life free from sin far more attractive than any sermon ever could.
-Satan, Christ’s adversary, plotted throughout Jesus’ earthly life to seduce Him into sin. In the wilderness the Devil unleashed his fiercest assault against His integrity (Matthew 4:1-11). In Gethsemane before His crucifixion, the pressure of temptation reached such intensity, Jesus sweat drops of blood (Luke 22:44).
-But Christ stood firm against everything the Devil threw at Him-“yet without sin.” Because Jesus experienced the full range of human problems and temptations, He understands our struggles. He is able “to sympathize with our weaknesses” (Hebrews 4:15).
-Why was it necessary for Jesus to live a sinless life?
-“God made Jesus who was sinless, to be sin for us; and in place of our sinful life Jesus gives us His sinless life, that we might be made sinless in Him.”-2 Corinthians 5:21, paraphrased.
-Jesus overcame temptation and lived a sinless life so He could pass it on to us in exchange for our old life of sin.
-(3)	Jesus Died to Take Away Sin
-How many people have sinned?
-“For ALL have sinned and fall short of the glory of God.”-Romans 3:23.
-What is the penalty for sin?
-“The wages of sin is DEATH; but the gift of God is eternal life in Christ Jesus our Lord.”-Romans 6:23.
-Why did Jesus die?
-“Look, the Lamb of God, who TAKES AWAY THE SIN of the world!” (John 1:29).
-All of us have sinned and are subject to eternal death, but Jesus died in our place. He became “sin for us.” He paid the death penalty for us. His dying is a gift, and “the GIFT of God is ETERNAL LIFE in Christ Jesus our Lord” (Romans 6:23).
-Jesus laid down His perfect, righteous life as a gift of love to us. Love like that is almost beyond human comprehension. And because of His death “we have PEACE with God” (Romans 5:1).
-(4)	Jesus Arose From the Dead
-Jesus’ death on the cross wasn’t the end of His remarkable story. He couldn’t very well remain dead and be our Savior.
-“And if Christ has not been raised, your faith is futile; you are still in your sins. Then those also who have fallen asleep in Christ are lost.”-1 Corinthians 15:17, 18.
-Mohammed and Buddha have presented to the world some great philosophical truths. They have inspired the lives of millions of people, but they have no supernatural power to give life since they still remain their graves. 
-Because Jesus rose from the tomb the third day after His death, what promise is He able to make to us?
-“Because I live, you also will live.”-John 14:19.
-Jesus is alive! Because He has a power over death, He can deliver us from death and offer us life that is both abundant and eternal. He will live in our hearts if we invite Him in. The risen Christ is present to meet our needs today.
-“Surely I am with you always, to the very end of the age.”-Matthew 28:20.
-Men and women all over the world are sharing stories of how Christ has delivered them from the worst addictions and the deepest emotional traumas. 
-One of our former students wrote these words of gratitude on one of his answer sheets: “I was an alcoholic. One day while drunk, I saw a card in the gutter advertising your Bible course. I picked it up, filled it out, and received my first true knowledge of Christ. Shortly after taking the Bible course, I offered my heart to God and lost the taste for whiskey.”
-When Jesus took possession of this man’s life, a new power gave him the ability to overcome his addiction. Because Christ is the risen Savior, He can save all who will come to Him for help.
-(5)	Jesus Ascended to Heaven
-Before Jesus returned to the Father after His resurrection (Acts 1:9), He made this promise to His followers:
-“Do not let your hearts be troubled. Trust in God; trust also in me. In my Father’s house are many rooms … I am going there TO PREPARE A PLACE FOR YOU. And … I will come back and take you to be with me… where I am.”-John 14:1-3.
-(6)	Jesus Ministers as Heavenly Priest
-Jesus constantly seeks to prepare us for a place in heaven.
-“He had to be made like his brothers in every way, in order that he might become a merciful and faithful HIGH PRIEST in service to God, and that he might make atonement for the sins of the people. Because he himself suffered when he was tempted, he is able to help those who are being tempted.”-Hebrews 2:17-18.
-Jesus came into our world to “make atonement for the sins of the people,” and rescue us from the misery of enslavement to sin. He died to save us so He can ultimately eradicate the cause of sin, suffering, and death by destroying the Devil.
-Jesus as our High Priest was “made like his brothers in every way.” And He now appears continually in the Father’s presence on our behalf as our Mediator. The same Jesus who blessed the children, rehabilitated the woman caught in adultery, and forgave the dying thief on the cross, is working in heaven right now to minister to our needs, “to help those who are being tempted.”
-(7)	Jesus Will Return
-Before returning to heaven, what promise did Jesus make?
-“If I go and prepare a place for you, I WILL COME BACK and take you to be with me that you also may be where I am.”-John 14:3.
-When Jesus returns, He will deliver us from the sin, sickness, tragedy, and death that plague this planet. And He will welcome us into a new world of eternal happiness and immortal life.
 
             `,
-            questions: [
-            
-            ]
+            audioPath: './stories/audio/Section 4/S04E04.mp3',
+            questions: []
         },
         {
             text: `
-                <h2>UNFAILING LOVE</h2>
-The story is told of an arranged marriage in Taiwan between U Long and a young woman named “Golden Flower.” When U Long lifted the veil from his bride after the ceremony, he was shocked and disgusted. Her face had been scarred by smallpox.
-After that, U Long had as little to do with his wife as possible. She tried her best to make him happy; she worked hard at home, hoping her husband would eventually accept her. But he remained coldly different to all her expressions of affection.
-After twelve years of this sham marriage, U Long began to lose vision in both eyes. A doctor told him would go completely blind if he didn’t have a cornea transplant. But the operation was expensive and there was a long waiting list.
-Golden Flower began to work long hours in the evening making straw hats for extra money. One day, U Long was informed that someone’s cornea had been made available after an accident. He rushed to the hospital to have the operation.
-After he had recovered, he grudgingly decided to see his wife so he could thank her for raising the money. When he turned her bowed head so she could look at him, U Long gasped. She stared at him with sightless, blank eyes, the cornea gone. Overcome with emotion, he sank at her feet and sobbed. And then for the first time he whispered her name: Golden Flower.
-Jesus longs for a relationship with those who’ve been indifferent to Him for so long. He longs for us to finally whisper His name as our Savior. He was willing to sacrifice not only His eyes but His whole body in order to demonstrate His unfailing love. His love is so powerful that Christ “came into the world to save sinners” (Timothy 1:15).
-JESUS
-CAME as God in human flesh.
-LIVED a sinless life in our place.
-DIED for our sins.
-AROSE to deliver us from death.
-ASCENDED to prepare a home for us in heaven.
-MINISTERS each day as our high priest.
-IS COMING SOON to take us to be with Him forever.
-Christ’s great sacrifice has created a bridge that spans our indifference, that covers our alienation. Have 
-you personally discovered
-that He wants to draw you
-over the chasm and into His 
-arms? Will you respond and 
-pray, “Jesus, I love You.
-Thank You for Your incredible
-sacrifice. Come into my heart 
-and save me now-save me wholly, save me completely, save me eternally”?
 
             `,
-            questions: [
-            
-            ]
+            audioPath: './stories/audio/Section 4/S04E05.mp3',
+            questions: []
         }
+
     ]
 };
 
